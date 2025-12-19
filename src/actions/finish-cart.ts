eaeffdcbecb03c61d8dec56f742359acfc84c9e5
victhorhugoo <victhorhugo.dev@gmail.com>
2025-12-19 14:58:15 -0300

    feat: cart: finish purchase button

---
+++ src/actions/finish-cart.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { CartItem } from "@/types/cart-item";
+
+export const finishCart = async (
+  token: string,
+  addressId: number,
+  cart: CartItem[]
+) => {
+  //t
+  return "https://www.google.com";
+};
--- src/components/cart/finish-purchase-button.tsx
+++ src/components/cart/finish-purchase-button.tsx
@@ -1,6 +1,56 @@
+"use client";
+
+import { clearCartCookie } from "@/actions/clear-cart-cookie";
+import { finishCart } from "@/actions/finish-cart";
+import { useAuthStore } from "@/store/auth";
+import { useCartStore } from "@/store/cart";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
 export const FinishPurchaseButton = () => {
+  const { token, hydrated } = useAuthStore((state) => state);
+
+  const cartStore = useCartStore((state) => state);
+
+  const handleFinishButton = async () => {
+    if (!token || !cartStore.selectedAddressId) return;
+
+    const sessionUrl = await finishCart(
+      token,
+      cartStore.selectedAddressId,
+      cartStore.cart
+    );
+
+    if (sessionUrl) {
+      await clearCartCookie();
+      cartStore.clearCart();
+      redirect(sessionUrl);
+    } else {
+      alert("Erro ao finalizar compra");
+    }
+  };
+
+  if (!hydrated) {
+    return null;
+  }
+
+  if (!token) {
+    return (
+      <Link
+        href={"/login"}
+        className="block bg-blue-600 w-full text-center px-6 py-5 text-white border-0 rounded-sm"
+      >
+        Faça login para finalizar
+      </Link>
+    );
+  }
+
   return (
-    <button className="bg-blue-600 w-full text-center px-6 py-5 text-white border-0 rounded-sm">
+    <button
+      disabled={!cartStore.selectedAddressId ? true : false}
+      onClick={handleFinishButton}
+      className="cursor-pointer bg-blue-600 w-full text-center px-6 py-5 text-white border-0 rounded-sm disabled:opacity-30"
+    >
       Finalizar compra
     </button>
   );
